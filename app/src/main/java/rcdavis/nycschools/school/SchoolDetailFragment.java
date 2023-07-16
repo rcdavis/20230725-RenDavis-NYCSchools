47f9f2c3319075f00f9c5615d36255a7d6095ee8
rcdavis <rcdavisgd@gmail.com>
2023-07-15 22:31:03 -0400

    Added school list and detail fragments

---
+++ app/src/main/java/rcdavis/nycschools/school/SchoolDetailFragment.java
@@ -0,0 +1,49 @@
+package rcdavis.nycschools.school;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import rcdavis.nycschools.BaseFragment;
+import rcdavis.nycschools.databinding.FragmentSchoolDetailBinding;
+import rcdavis.nycschools.util.MockData;
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a {@link SchoolListFragment}
+ * in two-pane mode (on larger screen devices) or self-contained
+ * on handsets.
+ */
+public class SchoolDetailFragment
+        extends BaseFragment<SchoolViewModel, FragmentSchoolDetailBinding> {
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public SchoolDetailFragment() {}
+
+    @Override
+    protected void onInit(Bundle savedInstanceState) {
+        addDisposable(viewModel.getSelectedSchool()
+                .onErrorReturn(e -> {
+                    e.printStackTrace();
+                    return MockData.getSchools().get(0);
+                })
+                .subscribe(school -> binding.setSchool(school)));
+    }
+
+    @Override
+    protected Class<SchoolViewModel> getViewModelClass() {
+        return SchoolViewModel.class;
+    }
+
+    @Override
+    protected FragmentSchoolDetailBinding createViewBinding(
+            @NonNull LayoutInflater inflater, ViewGroup container
+    ) {
+        return FragmentSchoolDetailBinding.inflate(inflater, container, false);
+    }
+}
---
+++ app/src/main/java/rcdavis/nycschools/school/SchoolListFragment.java
@@ -0,0 +1,57 @@
+package rcdavis.nycschools.school;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
+
+import rcdavis.nycschools.BaseFragment;
+import rcdavis.nycschools.R;
+import rcdavis.nycschools.databinding.FragmentSchoolListBinding;
+
+/**
+ * A fragment representing a list of Items. This fragment
+ * has different presentations for handset and larger screen devices. On
+ * handsets, the fragment presents a list of items, which when touched,
+ * lead to a {@link SchoolDetailFragment} representing
+ * item details. On larger screens, the Navigation controller presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class SchoolListFragment extends BaseFragment<SchoolViewModel, FragmentSchoolListBinding> {
+    @Override
+    protected Class<SchoolViewModel> getViewModelClass() {
+        return SchoolViewModel.class;
+    }
+
+    @Override
+    protected FragmentSchoolListBinding createViewBinding(
+            @NonNull LayoutInflater inflater, ViewGroup container
+    ) {
+        return FragmentSchoolListBinding.inflate(inflater, container, false);
+    }
+
+    @Override
+    protected void onInit(Bundle savedInstanceState) {
+        final SchoolRecyclerViewAdapter adapter = new SchoolRecyclerViewAdapter();
+        binding.itemList.setAdapter(adapter);
+
+        addDisposable(viewModel.getAllSchools()
+                .subscribe(adapter::setItems));
+
+        adapter.onViewClicked()
+                .subscribe(clickedView -> onClickView(clickedView.view, clickedView.item));
+    }
+
+    private void onClickView(final View view, final School school) {
+        viewModel.updateSelectedSchool(school);
+        if (binding.itemDetailNavContainer != null) {
+            Navigation.findNavController(binding.itemDetailNavContainer)
+                    .navigate(R.id.fragment_item_detail);
+        } else {
+            Navigation.findNavController(view).navigate(R.id.show_item_detail);
+        }
+    }
+}
